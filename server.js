b3260998ad5a075f62f98f9083cf55a4bc935c2b
djtouchette <djtouchette1993@gmail.com>
2016-08-29 15:09:49 -0700

    new build

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const outputPath = path.resolve(process.cwd(), 'build');
   app.use(publicPath, express.static(outputPath));
 
   app.get('/', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
-  app.get('/45.60ec5b75f07a1656e681.chunk.js', => res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js')));
-  
+  app.get('/45.60ec5b75f07a1656e681.chunk.js', (req, res) => res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js')));
+
   const port = 62102;
 
   app.listen(port, (err) => {
