e93f693122b3d75a3259b0ddabd1f3c428638a8b
djtouchette <djtouchette1993@gmail.com>
2016-08-29 10:48:42 -0700

    new build

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
-const resolve = require('path').resolve;
+const path = require('path');
+const compression = require('compression');
 
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/build/index.html`)
-});
+// Production middlewares
+const publicPath = '/';
+const outputPath = path.resolve(process.cwd(), 'build');
 
-app.use(express.static(__dirname));
+  // compression middleware compresses your server responses which makes them
+  // smaller (applies also to assets). You can read more about that technique
+  // and other good practices on official Express.js docs http://mxs.is/googmy
+  app.use(compression());
+  app.use(publicPath, express.static(outputPath));
 
-app.listen(3000, () => {
-  console.log(`listening on port 3000`);
-  console.log(`${__dirname}/build/index.html`);
-});
+  app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
+
+  const port = 62102;
+
+  app.listen(port, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`listening on port ${port}`);
+  });
