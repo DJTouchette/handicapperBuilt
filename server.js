44a06ad912aa860dd2648268ba6fee16f590fb5f
djtouchette <djtouchette1993@gmail.com>
2016-08-30 13:24:52 -0700

    dsdds

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ const productionOutput = 'https://misc-rnnukmcdu8mee.netdna-ssl.com/tsm/admin/bu
   app.use(publicPath, express.static(outputPath));
 
   app.get('/admin/', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
-  app.get('/admin/build/main.*.js', (req, res) => {console.log('haaaaay'); res.sendFile(path.resolve(outputPath, 'main.7e604d806c347d0050f1.js'))});
-  app.get('/admin/build/vendor.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'vendor.4338e5b794c25e63f6cb.js'))});
-  app.get('/admin/build/main.*.css', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.d84f944b166f9128a9bac6169e27aab1.css'))});
-  app.get('/admin/build/45.*.chunk.js', (req, res) => {res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js'))});
+  // app.get('/admin/build/main.*.js', (req, res) => {console.log('haaaaay'); res.sendFile(path.resolve(outputPath, 'main.7e604d806c347d0050f1.js'))});
+  // app.get('/admin/build/vendor.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'vendor.4338e5b794c25e63f6cb.js'))});
+  // app.get('/admin/build/main.*.css', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.d84f944b166f9128a9bac6169e27aab1.css'))});
+  // app.get('/admin/build/45.*.chunk.js', (req, res) => {res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js'))});
   app.get('/admin/appcache/manifest.html', (req, res) => {res.sendFile(path.resolve(outputPath, 'appcache/manifest.html'))});
   app.get('/admin/appcache/manifest.appcache', (req, res) => {res.sendFile(path.resolve(outputPath, 'appcache/manifest.appcache/'))});
 
   // app.get('/admin/45.*.chunk.js', (req, res) => {res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js'))});
-  app.get('/admin/build/:file', (req, res, next) {
+  app.get('/admin/build/:file', (req, res, next) => {
     let file = req.params.file;
     let filePath = path.resolve(outputPath, file);
     res.sendFile(path.resolve(outputPath, filePath));
