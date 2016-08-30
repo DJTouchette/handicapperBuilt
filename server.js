ee6041351f950cabfecf283383bcd3fd4c79400d
djtouchette <djtouchette1993@gmail.com>
2016-08-30 12:53:40 -0700

    dsdds

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ const productionOutput = 'https://misc-rnnukmcdu8mee.netdna-ssl.com/tsm/admin/bu
   app.get('/admin/build/main.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.7e604d806c347d0050f1.js'))});
   app.get('/admin/build/vendor.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'vendor.4338e5b794c25e63f6cb.js'))});
   app.get('/admin/build/main.*.css', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.d84f944b166f9128a9bac6169e27aab1.css'))});
-  // app.get('/admin/build/45.60ec5b75f07a1656e681.chunk.js', (req, res) => {res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js'))});
-  // app.get('/admin/appcache/manifest.html', (req, res) => {res.sendFile(path.resolve(outputPath, '/appcache/manifest.html'))});
-
-   Failed to load resource: the server responded with a
+  app.get('/admin/build/45.60ec5b75f07a1656e681.chunk.js', (req, res) => {res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js'))});
   const port = 62102;
 
   app.listen(port, (err) => {
