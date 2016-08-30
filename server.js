81adf6c11ebbab51065dea8efb6edf482912c19f
djtouchette <djtouchette1993@gmail.com>
2016-08-30 11:32:48 -0700

    dsdds

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const productionOutput = 'https://misc-rnnukmcdu8mee.netdna-ssl.com/tsm/admin/bu
   app.use(publicPath, express.static(outputPath));
 
   app.get('/admin/', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
-  app.get('/admin/build/main.2c59f7e7c57d21dba922.js ', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.2c59f7e7c57d21dba922.js'))});
+  app.get('/admin/build/main.*.js ', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.41f0d908d2ed1f2b550c.js'))});
   app.get('/admin/build/vendor.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'vendor.ffc794352051bdfc501c.js'))});
   app.get('/admin/build/main.*.css', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.d84f944b166f9128a9bac6169e27aab1.css'))});
 
