fc9748febf37e2d80129215d8ef60560ad5dc7ec
djtouchette <djtouchette1993@gmail.com>
2016-08-30 11:44:36 -0700

    dsdds

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const productionOutput = 'https://misc-rnnukmcdu8mee.netdna-ssl.com/tsm/admin/bu
   app.get('/admin/build/main.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.41f0d908d2ed1f2b550c.js'))});
   app.get('/admin/build/vendor.*.js', (req, res) => {res.sendFile(path.resolve(outputPath, 'vendor.ffc794352051bdfc501c.js'))});
   app.get('/admin/build/main.*.css', (req, res) => {res.sendFile(path.resolve(outputPath, 'main.d84f944b166f9128a9bac6169e27aab1.css'))});
-
+  app.get('/admin/build45.60ec5b75f07a1656e681.chunk.js', (req, res) => {res.sendFile(path.resolve(outputPath, '45.60ec5b75f07a1656e681.chunk.js'))});
   const port = 62102;
 
   app.listen(port, (err) => {
